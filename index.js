f1d19b95c86c3806329abb2968a07c8304a2ca50
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-08-23 16:51:11 -0500

    primera prueba

---
+++ index.js
@@ -0,0 +1,21 @@
+const Express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const mongoose = require('./database');
+
+const PORT = process.env.PORT || 3000;
+const app = Express();
+
+//middelwares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(Express.json());
+
+//router
+app.use('/api/task', require('./routes/task.routes'));
+
+//server
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
---
+++ routes/task.routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Task = require('../models/tasks');
+
+//get tasks
+router.get('/', async (req, res)=>{
+    const tarea = await Task.find();
+    res.json(tarea);
+})
+
+router.get('/:id', async (req, res)=>{
+    const tarea = await Task.findById(req.params.id);
+    res.json(tarea);
+})
+
+//create tasks
+router.post('/', async (req, res)=>{
+    const {title, description}=req.body;
+    const task = new Task({title, description});
+    await task.save();
+    res.json({status: 'task created'});    
+})
+
+router.put('/:id', async (req, res) => {
+    const {title, description}= req.body;
+    const newTask={title, description};
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({status: 'Tarea actualizada'});
+})
+
+//delete
+router.delete('/:id', async (req, res)=>{
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status: "Tarea eliminada"})
+})
+
+module.exports = router;
